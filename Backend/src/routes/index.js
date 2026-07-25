5db91dc8e35411b71b9a15b4a74bc89fc23ecc0a
Jeam <161737374+JeamGr@users.noreply.github.com>
2026-07-25 10:51:28 -0500

    Revert "actualizacion login y register"

--- Backend/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-// src/routes/index.js
-const { Router } = require('express');
-const apiV1 = require('./api');
-
-const router = Router();
-
-router.use('/api/v1', apiV1);
-
-module.exports = router;
